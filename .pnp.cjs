ea8de66e7a25845978ca79bad3757be6e4f51f03
wootaiklee <fined000@naver.com>
2024-03-17 11:35:28 +0900

    카카오 공유하기, sns 로그인

--- .pnp.cjs
+++ .pnp.cjs
@@ -41,6 +41,7 @@ const RAW_RUNTIME_STATE =
           ["@types/jest", "npm:27.5.2"],\
           ["@types/node", "npm:16.18.87"],\
           ["@types/react", "npm:18.2.64"],\
+          ["@types/react-copy-to-clipboard", "npm:5.0.7"],\
           ["@types/react-dom", "npm:18.2.21"],\
           ["craco-alias", "npm:3.0.1"],\
           ["date-fns", "npm:3.3.1"],\
@@ -52,6 +53,7 @@ const RAW_RUNTIME_STATE =
           ["firebase", "npm:10.8.1"],\
           ["prettier", "npm:3.2.5"],\
           ["react", "npm:18.2.0"],\
+          ["react-copy-to-clipboard", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:5.1.0"],\
           ["react-dom", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:18.2.0"],\
           ["react-infinite-scroll-component", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:6.1.0"],\
           ["react-markdown", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:9.0.1"],\
@@ -6677,6 +6679,16 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["@types/react-copy-to-clipboard", [\
+      ["npm:5.0.7", {\
+        "packageLocation": "../../../.yarn/berry/cache/@types-react-copy-to-clipboard-npm-5.0.7-cf9459d351-10c0.zip/node_modules/@types/react-copy-to-clipboard/",\
+        "packageDependencies": [\
+          ["@types/react-copy-to-clipboard", "npm:5.0.7"],\
+          ["@types/react", "npm:18.2.64"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["@types/react-dom", [\
       ["npm:18.2.21", {\
         "packageLocation": "../../../.yarn/berry/cache/@types-react-dom-npm-18.2.21-7d063bfb04-10c0.zip/node_modules/@types/react-dom/",\
@@ -9087,6 +9099,16 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "../../../.yarn/berry/cache/cookie-signature-npm-1.0.6-93f325f7f0-10c0.zip/node_modules/cookie-signature/",\
         "packageDependencies": [\
           ["cookie-signature", "npm:1.0.6"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["copy-to-clipboard", [\
+      ["npm:3.3.3", {\
+        "packageLocation": "../../../.yarn/berry/cache/copy-to-clipboard-npm-3.3.3-6964e6cfad-10c0.zip/node_modules/copy-to-clipboard/",\
+        "packageDependencies": [\
+          ["copy-to-clipboard", "npm:3.3.3"],\
+          ["toggle-selection", "npm:1.0.6"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -15306,6 +15328,7 @@ const RAW_RUNTIME_STATE =
           ["@types/jest", "npm:27.5.2"],\
           ["@types/node", "npm:16.18.87"],\
           ["@types/react", "npm:18.2.64"],\
+          ["@types/react-copy-to-clipboard", "npm:5.0.7"],\
           ["@types/react-dom", "npm:18.2.21"],\
           ["craco-alias", "npm:3.0.1"],\
           ["date-fns", "npm:3.3.1"],\
@@ -15317,6 +15340,7 @@ const RAW_RUNTIME_STATE =
           ["firebase", "npm:10.8.1"],\
           ["prettier", "npm:3.2.5"],\
           ["react", "npm:18.2.0"],\
+          ["react-copy-to-clipboard", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:5.1.0"],\
           ["react-dom", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:18.2.0"],\
           ["react-infinite-scroll-component", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:6.1.0"],\
           ["react-markdown", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:9.0.1"],\
@@ -18063,6 +18087,30 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["react-copy-to-clipboard", [\
+      ["npm:5.1.0", {\
+        "packageLocation": "../../../.yarn/berry/cache/react-copy-to-clipboard-npm-5.1.0-de9742f2bc-10c0.zip/node_modules/react-copy-to-clipboard/",\
+        "packageDependencies": [\
+          ["react-copy-to-clipboard", "npm:5.1.0"]\
+        ],\
+        "linkType": "SOFT"\
+      }],\
+      ["virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:5.1.0", {\
+        "packageLocation": "./.yarn/__virtual__/react-copy-to-clipboard-virtual-ba406c95e9/4/.yarn/berry/cache/react-copy-to-clipboard-npm-5.1.0-de9742f2bc-10c0.zip/node_modules/react-copy-to-clipboard/",\
+        "packageDependencies": [\
+          ["react-copy-to-clipboard", "virtual:d0f7a050e177013e889acb3ef375dbc6426d0e1f298d81febc98c7ea58f90703b888495f15ade0352b8ff053c9fbf5f06e27d4539c367419281bd62ed356c7c0#npm:5.1.0"],\
+          ["@types/react", "npm:18.2.64"],\
+          ["copy-to-clipboard", "npm:3.3.3"],\
+          ["prop-types", "npm:15.8.1"],\
+          ["react", "npm:18.2.0"]\
+        ],\
+        "packagePeers": [\
+          "@types/react",\
+          "react"\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["react-dev-utils", [\
       ["npm:12.0.1", {\
         "packageLocation": "../../../.yarn/berry/cache/react-dev-utils-npm-12.0.1-83ba06e3ee-10c0.zip/node_modules/react-dev-utils/",\
@@ -20170,6 +20218,15 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["to-regex-range", "npm:5.0.1"],\
           ["is-number", "npm:7.0.0"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["toggle-selection", [\
+      ["npm:1.0.6", {\
+        "packageLocation": "../../../.yarn/berry/cache/toggle-selection-npm-1.0.6-c506b73005-10c0.zip/node_modules/toggle-selection/",\
+        "packageDependencies": [\
+          ["toggle-selection", "npm:1.0.6"]\
         ],\
         "linkType": "HARD"\
       }]\
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Test from './pages/Test'
 import HotelList from './pages/HotelList'
 import HotelPage from './pages/Hotel'
+import useLoadKakao from './hooks/useLoadKakao'
+import MyPage from './pages/My'
+import SigninPage from './pages/Signin'
 function App() {
+  useLoadKakao()
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HotelList />} />
         <Route path="/test" element={<Test />} />
         <Route path="/hotel/:id" element={<HotelPage />} />
+        <Route path="/my" element={<MyPage />} />
+        <Route path="/signin" element={<SigninPage />} />
       </Routes>
     </BrowserRouter>
   )
